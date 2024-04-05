db1600d09a2d792867492d491ae29279ec66ab8b
Feliperaxd <lipeamaralsantos@gmail.com>
2024-04-04 23:59:23 -0300

    Doc update

--- docs/scripts/copy_button_engine.js
+++ docs/scripts/copy_button_engine.js
@@ -6,7 +6,9 @@ function clickCopyButton(clickedButton) {
     var buttons = document.getElementsByClassName('copy-button');
     for (var i = 0; i < buttons.length; i++) {
         buttons[i].style.animation = 'uncheckCopy 0.5s ease forwards';
-        buttons[i].style.animation = 'none';
+        setTimeout(function() {
+            buttons[i].style.animation = 'none';
+        }, 500);
     }
     clickedButton.style.animation = 'checkCopy 0.5s ease forwards';
 }
--- docs/scripts/header_engine.js
+++ docs/scripts/header_engine.js
@@ -1,10 +1,14 @@
-window.addEventListener('scroll', function(){
-    var headerElement = document.querySelector('header');
-    if (window.scrollY > 0) {
-      headerElement.style.height = '1000px'
+const iframe = document.getElementById('main-iframe');
+const mainHeader = document.getElementById('main-header');
+
+iframe.contentWindow.addEventListener('scroll', function () {
+    if (iframe.contentWindow.pageYOffset > 70) {
+      mainHeader.style.animation = 'collapseHeader 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275) forwards';
+      iframe.style.animation = 'expandIframe 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275) forwards';
     } 
     else {
-      headerElement.style.height = '100px'
+      mainHeader.style.animation = 'expandHeader 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275) forwards';
+      iframe.style.animation = 'collapseIframe 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275) forwards';
     }
   }
 );
